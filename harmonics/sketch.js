88d438b18847d9cf4d52ffe2498ec344d3e42997
robinp <44908176+RPDevGit@users.noreply.github.com>
2018-12-26 18:01:15 +0100

    whyyyy

--- harmonics/sketch.js
+++ harmonics/sketch.js
@@ -13,7 +13,7 @@ function setup() {
       	harmonics[harmonics.length] = parseInt(textboxvalue);
       	let showstring = harmonics.map(String);	
       	document.getElementById("textfield").value = showstring;
-      	document.getElementById("textbox").value = null;
+      	document.getElementById("textbox").value = showstring;
       
       	console.log(harmonics);
       
